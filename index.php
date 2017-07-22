7025f12ac5d18fcf955f3a2d4c8592521131fac4
Arthur Freiberger <xpigue@gmail.com>
2017-07-22 15:06:57 +1000

    no message

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<header class="header">
+	</header>
+	<div class="container">
+		<div class="wordsContainer">
+			<div class="row 1">
+				<p class="char word word1">A</p>
+				<p class="char">B</p>
+				<p class="char charReverse">C</p>
+				<p class="char">D</p>
+				<p class="char">E</p>
+				<p class="char">R</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 2">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char">C</p>
+				<p class="char charReverse word">C</p>
+				<p class="char">E</p>
+				<p class="char">E</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char charReverse">I</p>
+			</div>
+			<div class="row 3">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char">C</p>
+				<p class="char charReverse word">O</p>
+				<p class="char">S</p>
+				<p class="char word">R</p>
+				<p class="char charReverse">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 4">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char">C</p>
+				<p class="char charReverse word">R</p>
+				<p class="char word">S</p>
+				<p class="char word">E</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 5">
+				<p class="char charReverse">A</p>
+				<p class="char">B</p>
+				<p class="char charReverse">C</p>
+				<p class="char word">P</p>
+				<p class="char word">O</p>
+				<p class="char word">S</p>
+				<p class="char charReverse">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 6">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char">C</p>
+				<p class="char word">O</p>
+				<p class="char word">C</p>
+				<p class="char word">P</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 7">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char">C</p>
+				<p class="char word">R</p>
+				<p class="char word">I</p>
+				<p class="char word">O</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 8">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char charReverse">C</p>
+				<p class="char word">A</p>
+				<p class="char word">A</p>
+				<p class="char word">N</p>
+				<p class="char charReverse">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 9">
+				<p class="char">A</p>
+				<p class="char charReverse">B</p>
+				<p class="char">C</p>
+				<p class="char word charReverse">T</p>
+				<p class="char word">L</p>
+				<p class="char word">S</p>
+				<p class="char">G</p>
+				<p class="char charReverse">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 10">
+				<p class="char">A</p>
+				<p class="char charReverse">B</p>
+				<p class="char">C</p>
+				<p class="char word">E</p>
+				<p class="char charReverse">E</p>
+				<p class="char word">I</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char charReverse">I</p>
+			</div>
+			<div class="row 11">
+				<p class="char">A</p>
+				<p class="char charReverse">B</p>
+				<p class="char">C</p>
+				<p class="char charReverse">D</p>
+				<p class="char">E</p>
+				<p class="char word">B</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char charReverse">I</p>
+			</div>
+			<div class="row 12">
+				<p class="char">A</p>
+				<p class="char charReverse">B</p>
+				<p class="char">C</p>
+				<p class="char charReverse">D</p>
+				<p class="char">E</p>
+				<p class="char word">I</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char charReverse">I</p>
+			</div>
+			<div class="row 13">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char charReverse">C</p>
+				<p class="char">D</p>
+				<p class="char charReverse">E</p>
+				<p class="char word">L</p>
+				<p class="char charReverse">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+			<div class="row 14">
+				<p class="char">A</p>
+				<p class="char">B</p>
+				<p class="char">C</p>
+				<p class="char">D</p>
+				<p class="char">E</p>
+				<p class="char word">I</p>
+				<p class="char charReverse">G</p>
+				<p class="char">H</p>
+				<p class="char charReverse">I</p>
+			</div>
+			<div class="row 15">
+				<p class="char charReverse">A</p>
+				<p class="char">B</p>
+				<p class="char charReverse">C</p>
+				<p class="char">D</p>
+				<p class="char">E</p>
+				<p class="char word">T</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char charReverse">I</p>
+			</div>
+			<div class="row 16">
+				<p class="char">A</p>
+				<p class="char charReverse">B</p>
+				<p class="char">C</p>
+				<p class="char">D</p>
+				<p class="char charReverse">E</p>
+				<p class="char word">Y</p>
+				<p class="char">G</p>
+				<p class="char">H</p>
+				<p class="char">I</p>
+			</div>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {  
+  $('.word').hover(function(){
+    $(this).addClass('found');
+  });
+});
