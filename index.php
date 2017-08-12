de604c0d905d38fa349cb4f4446310ee4bdbff10
Arthur Freiberger <xpigue@gmail.com>
2017-08-13 08:15:08 +1000

    New Header implemented - nearly responsive; Words Game section is now working ok; Footer finished;

--- index.php
+++ index.php
@@ -5,217 +5,350 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<header class="header">
+	<!-- <header class="header">
+		<div class="logo"></div>
 		<nav class="menu">
 			<a href="#" class="menuBtn" title="About the Way360 Group">ABOUT US</a>
 			<a href="#" class="menuBtn" title="What is Corporate Social Responsibility?">WHAT IS CSR?</a>
-			<a href="#" class="menuBtn" title="What the Way360 Group does?">WHAT WE DO</a>
+			<a href="#" class="menuBtn" title="Latest Projects">LATEST PROJECTS</a>
 			<a href="#" class="menuBtn" title="Contact us">CONTACT</a>
 		</nav>
-	</header>
-	<section class="firstSection">
-			<div class="logoShadow1"></div>
-			<div class="logoShadow2"></div>
-			<div class="logo"></div>
-		<div class="titlesBox">
-			<div class="logo"></div>
-			<h1 class="weAre">We are the Way360 Group</h1>
-			<h2 class="weAreHere">And we’re here to help your business get more social.</h2>
-			<div class="socialIcons">
-				<a href="#" class="socialBtn facebook"></a>
-				<a href="#" class="socialBtn instagram"></a>
+	</header> -->
+
+	<div class="headerBtn">
+		<div class="line line1"></div>
+		<div class="line line2"></div>
+		<div class="line line3"></div>
+	</div>
+
+	<header class="headerOverlay">
+		<div class="logo"></div>
+		<nav class="menu">
+			<div class="menuBtnBox">
+				<a href="#" class="menuBtn" title="About the Way360 Group"><p>ABOUT US</p></a>
 			</div>
+			<div class="menuBtnBox">
+				<a href="#" class="menuBtn" title="What is Corporate Social Responsibility?"><p>WHAT IS CSR?</p></a>
+			</div>
+			<div class="menuBtnBox">
+				<a href="#" class="menuBtn" title="Latest Projects"><p>LATEST PROJECTS</p></a>
+			</div>
+			<div class="menuBtnBox">
+				<a href="#" class="menuBtn" title="Contact us"><p>CONTACT</p></a>
+			</div>
+		</nav>
+		<div class="meerkatsBox">
+			<div class="meerkats"></div>
+			<div class="globe"></div>
+			<div class="stripes"></div>
+		</div>
+	</header>
+
+	<section class="firstSection">
+		<div class="titlesBox">
+			<div class="weAreBox">
+				<h1 class="weAre">We are the Way360 Group</h1>
+			<!-- 
+				<span class="weAre">W</span>
+				<span class="weAre">e</span>
+				<span class="space"></span>
+				<span class="weAre">a</span>
+				<span class="weAre">r</span>
+				<span class="weAre">e</span>
+				<span class="space"></span>
+				<span class="weAre">t</span>
+				<span class="weAre">h</span>
+				<span class="weAre">e</span>
+				<span class="space"></span>
+				<span class="weAre">W</span>
+				<span class="weAre">a</span>
+				<span class="weAre">y</span>
+				<span class="weAre">3</span>
+				<span class="weAre">6</span>
+				<span class="weAre">0</span>
+				<span class="space"></span>
+				<span class="weAre">G</span>
+				<span class="weAre">r</span>
+				<span class="weAre">o</span>
+				<span class="weAre">u</span>
+				<span class="weAre">p</span>
+			-->
+			</div>
+			<h2 class="weAreHere">And we’re here to help your business get more social.</h2>
+		</div>
+		<!-- <div class="video" style="background-image: url('img/maxresdefault.png');" > -->
+		<div class="videoBox">
+			<div class="video">
+				<iframe width="100%" height="100%" id="video" src="https://www.youtube.com/embed/Ysyey8vwwHY" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="playBtn" id="playBtn">WATCH VIDEO</div>
 		</div>
 		<div class="meerkatsBox">
 			<div class="meerkats"></div>
 			<div class="globe"></div>
 			<div class="stripes"></div>
 		</div>
-		<div class="ellipsis">
-			<div class="ellipse"></div>
+		<div class="socialIcons">
+			<a href="#" class="socialBtn facebook"></a>
+			<a href="#" class="socialBtn instagram"></a>
 		</div>
 	</section>
-	<div class="container">
-		<div class="wordsContainer">
-			<div class="row 1">
-				<p class="char word word1">A</p>
-				<p class="char">B</p>
-				<p class="char charReverse">C</p>
-				<p class="char">D</p>
-				<p class="char">E</p>
-				<p class="char">R</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 2">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char">C</p>
-				<p class="char charReverse word">C</p>
-				<p class="char">E</p>
-				<p class="char">E</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char charReverse">I</p>
-			</div>
-			<div class="row 3">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char">C</p>
-				<p class="char charReverse word">O</p>
-				<p class="char">S</p>
-				<p class="char word">R</p>
-				<p class="char charReverse">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 4">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char">C</p>
-				<p class="char charReverse word">R</p>
-				<p class="char word">S</p>
-				<p class="char word">E</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 5">
-				<p class="char charReverse">A</p>
-				<p class="char">B</p>
-				<p class="char charReverse">C</p>
-				<p class="char word">P</p>
-				<p class="char word">O</p>
-				<p class="char word">S</p>
-				<p class="char charReverse">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 6">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char">C</p>
-				<p class="char word">O</p>
-				<p class="char word">C</p>
-				<p class="char word">P</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 7">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char">C</p>
-				<p class="char word">R</p>
-				<p class="char word">I</p>
-				<p class="char word">O</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 8">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char charReverse">C</p>
-				<p class="char word">A</p>
-				<p class="char word">A</p>
-				<p class="char word">N</p>
-				<p class="char charReverse">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 9">
-				<p class="char">A</p>
-				<p class="char charReverse">B</p>
-				<p class="char">C</p>
-				<p class="char word charReverse">T</p>
-				<p class="char word">L</p>
-				<p class="char word">S</p>
-				<p class="char">G</p>
-				<p class="char charReverse">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 10">
-				<p class="char">A</p>
-				<p class="char charReverse">B</p>
-				<p class="char">C</p>
-				<p class="char word">E</p>
-				<p class="char charReverse">E</p>
-				<p class="char word">I</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char charReverse">I</p>
-			</div>
-			<div class="row 11">
-				<p class="char">A</p>
-				<p class="char charReverse">B</p>
-				<p class="char">C</p>
-				<p class="char charReverse">D</p>
-				<p class="char">E</p>
-				<p class="char word">B</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char charReverse">I</p>
-			</div>
-			<div class="row 12">
-				<p class="char">A</p>
-				<p class="char charReverse">B</p>
-				<p class="char">C</p>
-				<p class="char charReverse">D</p>
-				<p class="char">E</p>
-				<p class="char word">I</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char charReverse">I</p>
-			</div>
-			<div class="row 13">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char charReverse">C</p>
-				<p class="char">D</p>
-				<p class="char charReverse">E</p>
-				<p class="char word">L</p>
-				<p class="char charReverse">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
-			</div>
-			<div class="row 14">
-				<p class="char">A</p>
-				<p class="char">B</p>
-				<p class="char">C</p>
-				<p class="char">D</p>
-				<p class="char">E</p>
-				<p class="char word">I</p>
-				<p class="char charReverse">G</p>
-				<p class="char">H</p>
-				<p class="char charReverse">I</p>
-			</div>
-			<div class="row 15">
-				<p class="char charReverse">A</p>
-				<p class="char">B</p>
-				<p class="char charReverse">C</p>
-				<p class="char">D</p>
-				<p class="char">E</p>
-				<p class="char word">T</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char charReverse">I</p>
-			</div>
-			<div class="row 16">
-				<p class="char">A</p>
-				<p class="char charReverse">B</p>
-				<p class="char">C</p>
-				<p class="char">D</p>
-				<p class="char charReverse">E</p>
-				<p class="char word">Y</p>
-				<p class="char">G</p>
-				<p class="char">H</p>
-				<p class="char">I</p>
+	
+	<section class="wordsGame">
+		<h2 class="sectionTitle">WHAT WE DO</h2>
+		<div class="container">
+			<div class="wordsContainer">
+				<div class="row 1">
+					<p class="char charReverse">H</p>
+					<p class="char">B</p>
+					<p class="char charReverse">C</p>
+					<p class="char">Z</p>
+					<p class="char charReverse">E</p>
+					<p class="char">X</p>
+					<p class="char">A</p>
+					<p class="char">H</p>
+					<p class="char charReverse">L</p>
+				</div>
+				<div class="row 2">
+					<p class="char">J</p>
+					<p class="char charReverse">D</p>
+					<p class="char">B</p>
+					<p class="char charReverse word">C</p>
+					<p class="char">H</p>
+					<p class="char charReverse">R</p>
+					<p class="char">L</p>
+					<p class="char">P</p>
+					<p class="char charReverse">S</p>
+				</div>
+				<div class="row 3">
+					<p class="char charReverse">U</p>
+					<p class="char">P</p>
+					<p class="char">B</p>
+					<p class="char charReverse word">O</p>
+					<p class="char">D</p>
+					<p class="char word">R</p>
+					<p class="char charReverse">G</p>
+					<p class="char charReverse">A</p>
+					<p class="char">X</p>
+				</div>
+				<div class="row 4">
+					<p class="char">X</p>
+					<p class="char word2">W</p>
+					<p class="char charReverse">H</p>
+					<p class="char charReverse word">R</p>
+					<p class="char word">S</p>
+					<p class="char word">E</p>
+					<p class="char">G</p>
+					<p class="char">P</p>
+					<p class="char charReverse">I</p>
+				</div>
+				<div class="row 5">
+					<p class="char charReverse">B</p>
+					<p class="char charReverse word2">O</p>
+					<p class="char">T</p>
+					<p class="char charReverse word">P</p>
+					<p class="char word">O</p>
+					<p class="char word">S</p>
+					<p class="char charReverse">Q</p>
+					<p class="char">W</p>
+					<p class="char">N</p>
+				</div>
+				<div class="row 6">
+					<p class="char charReverse">F</p>
+					<p class="char charReverse word2">R</p>
+					<p class="char">P</p>
+					<p class="char word">O</p>
+					<p class="char word">C</p>
+					<p class="char charReverse word">P</p>
+					<p class="char">G</p>
+					<p class="char charReverse">V</p>
+					<p class="char">J</p>
+				</div>
+				<div class="row 7">
+					<p class="char">Y</p>
+					<p class="char word2">K</p>
+					<p class="char charReverse">D</p>
+					<p class="char charReverse word">R</p>
+					<p class="char charReverse word">I</p>
+					<p class="char word">O</p>
+					<p class="char">L</p>
+					<p class="char charReverse word3">S</p>
+					<p class="char">W</p>
+				</div>
+				<div class="row 8">
+					<p class="char">Q</p>
+					<p class="char charReverse word2">S</p>
+					<p class="char">F</p>
+					<p class="char word">A</p>
+					<p class="char word">A</p>
+					<p class="char word">N</p>
+					<p class="char charReverse">C</p>
+					<p class="char word3">O</p>
+					<p class="char">U</p>
+				</div>
+				<div class="row 9">
+					<p class="char">A</p>
+					<p class="char word2">H</p>
+					<p class="char charReverse">H</p>
+					<p class="char word charReverse">T</p>
+					<p class="char word">L</p>
+					<p class="char word">S</p>
+					<p class="char">J</p>
+					<p class="char charReverse word3">C</p>
+					<p class="char">M</p>
+				</div>
+				<div class="row 10">
+					<p class="char">T</p>
+					<p class="char word2">O</p>
+					<p class="char charReverse">Y</p>
+					<p class="char word">E</p>
+					<p class="char charReverse">E</p>
+					<p class="char word">I</p>
+					<p class="char">S</p>
+					<p class="char word3">I</p>
+					<p class="char charReverse word3">G</p>
+				</div>
+				<div class="row 11">
+					<p class="char">L</p>
+					<p class="char word2">P</p>
+					<p class="char charReverse">O</p>
+					<p class="char charReverse">W</p>
+					<p class="char">N</p>
+					<p class="char word">B</p>
+					<p class="char">O</p>
+					<p class="char word3">A</p>
+					<p class="char charReverse word3">O</p>
+				</div>
+				<div class="row 12">
+					<p class="char">R</p>
+					<p class="char word2">S</p>
+					<p class="char charReverse">B</p>
+					<p class="char charReverse">N</p>
+					<p class="char">E</p>
+					<p class="char word">I</p>
+					<p class="char">D</p>
+					<p class="char word3">L</p>
+					<p class="char charReverse word3">O</p>
+				</div>
+				<div class="row 13">
+					<p class="char">C</p>
+					<p class="char">A</p>
+					<p class="char charReverse">L</p>
+					<p class="char">P</p>
+					<p class="char charReverse">P</p>
+					<p class="char word">L</p>
+					<p class="char charReverse">A</p>
+					<p class="char">B</p>
+					<p class="char word3">D</p>
+				</div>
+				<div class="row 14">
+					<p class="char">Y</p>
+					<p class="char">L</p>
+					<p class="char">H</p>
+					<p class="char">M</p>
+					<p class="char">A</p>
+					<p class="char word">I</p>
+					<p class="char charReverse">G</p>
+					<p class="char">U</p>
+					<p class="char charReverse">L</p>
+				</div>
+				<div class="row 15">
+					<p class="char charReverse">P</p>
+					<p class="char">A</p>
+					<p class="char charReverse">D</p>
+					<p class="char">E</p>
+					<p class="char">T</p>
+					<p class="char word">T</p>
+					<p class="char">K</p>
+					<p class="char">W</p>
+					<p class="char charReverse">A</p>
+				</div>
+				<div class="row 16">
+					<p class="char">U</p>
+					<p class="char charReverse">W</p>
+					<p class="char">D</p>
+					<p class="char charReverse">O</p>
+					<p class="char charReverse">P</p>
+					<p class="char word">Y</p>
+					<p class="char">A</p>
+					<p class="char">H</p>
+					<p class="char charReverse">M</p>
+				</div>
+				<div class="floatingLlama"></div>
+				<div class="wordsContainerBg"></div>
 			</div>
 		</div>
-	</div>
+	</section>
+	
+	<section class="projects">
+		<h2 class="sectionTitle">LATEST PROJECTS</h2>
+		<div class="videoBox">
+			<div class="video">
+				<iframe width="100%" height="100%" id="video" src="https://www.youtube.com/embed/Ysyey8vwwHY" frameborder="0" allowfullscreen></iframe>
+			</div>
+			<div class="playBtn">WATCH VIDEO</div>
+			<div class="monkey"></div>
+		</div>
+		<div class="bullets">
+			<div class="bullet"></div>
+			<div class="bullet"></div>
+			<div class="bullet"></div>
+			<div class="bullet"></div>
+		</div>
+	</section>
+
+	<section class="contact">
+		<div class="container">
+			<h2 class="sectionTitle">LET'S WORK TOGETHER</h2>
+			<div class="formContainer">
+				<div class="llamaContainer">
+					<div class="llama"></div>
+					<div class="globe"></div>
+					<div class="stripes"></div>
+				</div>
+				<form method="post" action="">
+					<div class="inputContainer">
+						<input type="text" class="input required" name="name" id="name" required>
+						<label for="name">Your name</label>
+						<div class="bottomBar"></div>
+					</div>
+					<div class="inputContainer">
+						<input type="email" class="input required" name="email" id="email" required>
+						<label for="email">Your email</label>
+						<div class="bottomBar"></div>
+					</div>
+					<div class="inputContainer">
+						<input type="number" class="input" name="phone" id="phone" required>
+						<label for="phone">Phone</label>
+						<div class="bottomBar"></div>
+					</div>
+					<div class="inputContainer">
+						<textarea class="textarea required" name="message" id="message" required></textarea>
+						<label for="message">Message</label>
+						<div class="bottomBar"></div>
+					</div>
+					<input type="submit" value="SEND" class="submit">
+				</form>
+			</div>
+		</div>
+	</section>
+
+	<footer class="footer">
+		<div class="container">
+			<p class="copyright">Copyright 2017 Way360Group</p>
+			<div class="meerkatsBox">
+				<div class="meerkats"></div>
+			</div>
+			<div class="logo"></div>
+			<div class="socialMedias">
+				<a href="http://www.facebook.com/Way360Group" target="_blank" class="socialBtn facebook"></a>
+				<a href="http://www.instagram.com/Way360Group" target="_blank" class="socialBtn instagram"></a>
+			</div>
+		</div>
+	</footer>
+
 	<script type="text/javascript" src="js/jquery.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript" src="js/youtube.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
 		$(this).addClass('found');
 	});
 
+	$('.word2').hover(function(){
+		$(this).addClass('found2');
+	});
+
+	$('.word3').hover(function(){
+		$(this).addClass('found3');
+	});
+
 	var movementStrength = 20;
 	var height = movementStrength / $(window).height();
 	var width = movementStrength / $(window).width();
@@ -10,18 +18,80 @@ $(document).ready(function() {
 	$(".firstSection").mousemove(function(e){
 	    var pageX = e.pageX - ($(window).width() / 600);
 	    var pageY = e.pageY - ($(window).height() / 600);
-	    var newvalueX = (width * pageX) / 4;
-	    var newvalueY = (height * pageY) / 4;
-     	var newvalueX2 = (width * pageX) / 2;
-	    var newvalueY2 = (height * pageY) / 2;
-	    //$('.logoS').css("background-position", "-" + newvalueX +"% -" + newvalueY +"%");
-	    //$(this).css("transform", "translate("+newvalueX+"px , "+newvalueY+"px)");
+	    var newvalueX = (width * pageX) / 2;
+	    var newvalueY = (height * pageY) / 2;
+     	var newvalueX2 = (width * pageX) / 4;
+	    var newvalueY2 = (height * pageY) / 4;
 	    $('.titlesBox').css("transform", "translate("+newvalueX2+"px , "+newvalueY2+"px)");
-	    $('.logoShadow1').css("transform", "translate(-"+newvalueX2+"px , -"+newvalueY2+"px)");
-	    $('.logoShadow2').css("transform", "translate("+newvalueX2+"px , "+newvalueY2+"px)");
 	    $('.logo').css("transform", "translate(-"+newvalueX2+"px , -"+newvalueY2+"px)");
-	    //$(this).css("transform", newvalueX +"% " + newvalueY +"%");
+	    $('.menu').css("transform", "translate(-"+newvalueX2+"px , -"+newvalueY2+"px)");
+	});
+
+	$(".wordsGame").mousemove(function(e){
+		var pageX = e.pageX - ($(window).width() / 600);
+	    var pageY = e.pageY - ($(window).height() / 500);
+		var newvalueX2 = (width * pageX);
+	    var newvalueY2 = (height * pageY) / 4;
+
+		$('.wordsContainerBg').css("transform", "translate(-"+newvalueX2+"px , -"+newvalueY2+"px)");
+	});
+
+	$('.headerBtn').click(function(f){
+		f.preventDefault();
+		$('.headerBtn').toggleClass('menuActive');
+		$('.headerOverlay').toggleClass('active');
+	})
+
+	$('.playBtn').click(function(e){
+
+		e.preventDefault();
+		video = $('.video');
+		//weAreTitle = $('.weAre');
+		weAreTitle = $('.weAreBox');
+		//$('.weAre').toggleClass('blurred');
+		//$('.weAreBox').toggleClass('transparent');
+		$('.header').toggleClass('lighterHeader');
+		$('.firstSection').toggleClass('darkFirstSection');
+		$('.weAreHere').toggleClass('transparent2');
+		$('.playBtn').toggleClass('playBtnShadow');
+		if ($('.weAreBox').hasClass('blurred')) {
+			console.log('tem');
+			$('.weAreBox').removeClass('blurred');
+		} else{
+			console.log('nao tem');
+			$('.weAreBox').addClass('blurred');
+		};
+		//$('.weAreHere').fadeToggle(500);
+		if (video.hasClass('videoScaled')) {
+			video.removeClass('videoScaled');
+		} else{
+			$('.video').addClass('videoScaled');
+		};
+		 
+	});
+
+	$(window).scroll(function(){
+
+		var height1 = $('.firstSection').height();
+		var height2 = $('.wordsGame').height() + 120;
+		var height3 = height2 + height1;
+
+		console.log(height3);
+
+		if ($(window).scrollTop() >= height1) {
+			console.log('OLA PEPA');
+			$(".headerBtn .line").css("background-color", "#FFF" );
+		} else {
+			$(".headerBtn .line").css("background-color", "#111" );	
+		}
+		if ($(window).scrollTop() >= height3) {
+			console.log('entrei');
+			$(".headerBtn .line").css("background-color", "#111" );
+		};
 
 	});
+
+
+
 
 });
